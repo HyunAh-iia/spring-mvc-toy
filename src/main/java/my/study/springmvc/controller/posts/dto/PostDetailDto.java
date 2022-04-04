8369de3b9f4f9e855e2170aa58fc9ad5b7c81e0a
iia <kha2464@gmail.com>
2022-04-04 09:02:45 +0900

    fix: 게시글 첨부파일이 비어있을 경우 null 대신 empty list 반환

--- src/main/java/my/study/springmvc/controller/posts/dto/PostDetailDto.java
+++ src/main/java/my/study/springmvc/controller/posts/dto/PostDetailDto.java
@@ -3,11 +3,19 @@ package my.study.springmvc.controller.posts.dto;
 import my.study.springmvc.model.posts.Post;
 
 import java.time.ZonedDateTime;
+import java.util.Collections;
 import java.util.List;
 
 public record PostDetailDto(Long id, String title, String content, List<String> fileLinks,
                             ZonedDateTime createdAt, ZonedDateTime updatedAt) {
     public static PostDetailDto of(Post post) {
         return new PostDetailDto(post.getId(), post.getTitle(), post.getContent(), post.getFileUrls(), post.getCreatedAt(), post.getUpdatedAt());
+    }
+
+    public List<String> getFileLinks() {
+        if (this.fileLinks == null) {
+            return Collections.emptyList();
+        }
+        return this.fileLinks;
     }
 }
