b49cee5cf97514227fe5b9080cc9f8343ddc63be
iia <kha2464@gmail.com>
2022-05-06 09:15:03 +0900

    feat: API에 대한 Prefix 추가 (RestController 대상)

--- src/main/java/my/study/springmvc/core/config/WebMvcConfig.java
+++ src/main/java/my/study/springmvc/core/config/WebMvcConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
@@ -36,5 +39,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
         AcceptHeaderLocaleResolver slr = new AcceptHeaderLocaleResolver();
         slr.setDefaultLocale(Locale.ENGLISH);
         return slr;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/v1",
+                HandlerTypePredicate.forAnnotation(RestController.class));
+        /*
+        Path Matching 에 대한 공식문서 : https://docs.spring.io/spring-framework/docs/current/reference/html/web.html#mvc-config-path-matching
+        고려했던 후보 옵션 : https://stackoverflow.com/a/67922089/13885206
+            - 선택하지 않은 이유 : @Controller 에도 동일하게 적용되기 때문
+        * */
     }
 }
